999abb61d480ec7ecdc90a2b836cf0fc75545586
Satish.Marathe <marathes_pune@yahoo.com>
2020-08-01 21:11:09 +1000

    first initial commit

---
+++ src/main/java/com/gettingstarted/springtxmgmt/SpringTxMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.gettingstarted.springtxmgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTxMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTxMgmtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gettingstarted/springtxmgmt/SpringTxMgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gettingstarted.springtxmgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTxMgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
