77e5078e23ea96bb85805bd14260908dbdc1233e
Satish.Marathe <marathes_pune@yahoo.com>
2020-08-02 11:48:53 +1000

    bypass datasource configuration for now to get away from startup exception

--- src/main/java/com/gettingstarted/springtxmgmt/SpringTxMgmtApplication.java
+++ src/main/java/com/gettingstarted/springtxmgmt/SpringTxMgmtApplication.java
@@ -2,8 +2,10 @@ package com.gettingstarted.springtxmgmt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class SpringTxMgmtApplication {
 
 	public static void main(String[] args) {
